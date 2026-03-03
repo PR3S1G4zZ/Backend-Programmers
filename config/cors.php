06dcd4e63973b1f6c85cb78838661faf694e0f4f
PR3S1G4 <presigadurangoesteban@gmail.com>
2026-03-03 12:05:55 -0500

    Update allowed_origins to include FRONTEND_URL
    Added environment variable for allowed origins in CORS.

--- config/cors.php
+++ config/cors.php
@@ -19,13 +19,13 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
+'allowed_origins' => [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
         'http://localhost:3000',
         'http://127.0.0.1:3000',
+        env('FRONTEND_URL', '*'), // <--- Esto leerá la variable de Railway o permitirá todo si no existe
     ],
-
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
